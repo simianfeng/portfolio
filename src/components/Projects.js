fb7b7c82376b47907d2a1666cc486723f223f9a6
simianfeng <56980681+simianfeng@users.noreply.github.com>
2020-06-22 20:49:51 -0400

    add remote

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,50 @@
-import React,{Component} from 'react';
+import React,{Component, useState} from 'react';
+import {Tabs, Tab} from 'react-mdl';
+
 const Projects=()=>{
+    const [activeTab, SetactiveTab] = useState(0);
+
+    const toggleCategories=()=>{
+         if (activeTab===0){
+            return(
+               <div>
+                   <h1>This is React</h1>
+               </div>
+            )
+        } else if (activeTab===1){
+            return(
+            <div>
+                <h1>This is Angular</h1>
+            </div>
+            )
+        } else if (activeTab===2){
+            return(
+            <div>
+                <h1>This is VueJS</h1>
+            </div>
+            )
+        } else if (activeTab===3){
+            return(
+            <div>
+                <h1>This is MongoDB</h1>
+            </div>
+            )
+        }
+        
+    }
+      
     return(
-        <div>
-            <h1>Projects</h1>
+        <div className='category-tabs'>
+            <Tabs activeTab = {activeTab} onChange={(tabId)=> SetactiveTab.tabId} ripple>
+                <Tab>React</Tab>
+                <Tab>Angular</Tab>
+                <Tab>VueJS</Tab>
+                <Tab>MongoDB</Tab>
+            </Tabs>
+
+            <section className='project-grid'>
+                {toggleCategories()}
+            </section>
         </div>
     )
 }
